df41f44b731246f0b0066004ff8def52a84e54b6
JesusGCastro <105814927+JesusGCastro@users.noreply.github.com>
2025-02-11 22:00:54 -0700

    Se realizo la siguiente parte, donde se crean listas de productos con la info y se intenta crear una vista con ello.

--- app/src/main/java/gonzalez/jesus/thecheezery/MenuActivity.kt
+++ app/src/main/java/gonzalez/jesus/thecheezery/MenuActivity.kt
@@ -1,6 +1,8 @@
 package gonzalez.jesus.thecheezery
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,6 +12,13 @@ class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+
+        var btnCold: Button = findViewById(R.id.buttton_cold_drinks) as Button
+
+        btnCold.setOnClickListener{
+            var intent: Intent = Intent(this, ProductosActivity::class.java)
+            startActivity(intent)
+        }
 
     }
 }
