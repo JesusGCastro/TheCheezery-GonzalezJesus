1359b6ae45c68027d1754aebe9864d91ff29e575
JesusGCastro <jesus.gonzalez247336@potros.itson.edu.mx>
2025-02-11 17:49:02 -0700

    Se agregaron imagenes nuevas, y se empezo la vista de productos

---
+++ app/src/main/java/gonzalez/jesus/thecheezery/ProductosActivity.kt
@@ -0,0 +1,15 @@
+package gonzalez.jesus.thecheezery
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ProductosActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_productos)
+
+    }
+}
